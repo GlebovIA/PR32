bd9e1953338fe7162ad2b198a6d7866e9bd64f4d
Kolovratik <Kolovratik@HOME-PC>
2024-04-15 09:17:13 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+
+namespace PR32
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenRecordList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenRecordAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportRecord(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacturersList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenManufacturersAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenSupplyAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateList(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenStateAdd(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
