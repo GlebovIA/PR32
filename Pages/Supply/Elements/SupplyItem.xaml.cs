a38463bd2d16f63f0c24f6cdd8d9664fbaa37c25
Kolovratik <Kolovratik@HOME-PC>
2024-05-03 08:20:07 +0300

    Разработанная страница отображения поставок

---
+++ Pages/Supply/Elements/SupplyItem.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PR32.Pages.Supply.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для SupplyItem.xaml
+    /// </summary>
+    public partial class SupplyItem : UserControl
+    {
+        IEnumerable<Classes.Manufacturer> AllManufacturers = Classes.Manufacturer.AllManufacturers();
+        IEnumerable<Classes.Record> AllRecords = Classes.Record.AllRecords();
+        Classes.Supply Supply;
+        Pages.Supply.SupplyMain Main;
+        public SupplyItem(Classes.Supply supply, SupplyMain main)
+        {
+            InitializeComponent();
+            Supply = supply;
+            Main = main;
+            ManufacturerTBx.Text = AllManufacturers.Where(x => x.Id == Supply.Manufacturer).First().Name;
+            RecordTBx.Text = AllRecords.Where(x => x.Id == Supply.Record).First().Name;
+            DateDeliveryTBx.Text = CorrectDate(Supply.DateDelivery);
+            CountTBx.Text = Supply.Count.ToString();
+        }
+        private string CorrectDate(string Value)
+        {
+            return Value.Split(' ')[0];
+        }
+
+        private void EditSupply(object sender, RoutedEventArgs e)
+        {
+            MainWindow.MW.OpenPages(new AddSupply(Supply));
+        }
+
+        private void DeleteSupply(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Удалить поставку №: {Supply.Id}?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Supply.Delete();
+                Main.ParentSP.Children.Remove(this);
+                MessageBox.Show($"Поставка №{Supply.Id} успешно удалена.", "Уведомление");
+            }
+        }
+    }
+}
---
+++ Pages/Supply/SupplyMain.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace PR32.Pages.Supply
+{
+    /// <summary>
+    /// Логика взаимодействия для SupplyMain.xaml
+    /// </summary>
+    public partial class SupplyMain : Page
+    {
+        public IEnumerable<Classes.Supply> AllSupplies = Classes.Supply.AllSupplies();
+        public SupplyMain()
+        {
+            InitializeComponent();
+            foreach (var supply in AllSupplies)
+                ParentSP.Children.Add(new Pages.Supply.Elements.SupplyItem(supply, this));
+        }
+    }
+}
